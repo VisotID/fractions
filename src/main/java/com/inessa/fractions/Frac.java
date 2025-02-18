7f10a5ea8f15c530355ba7c0212f86b5d15d9543
VisotID <inessav243@gmail.com>
2025-02-19 04:25:59 +0900

    update fx version

--- src/main/java/com/inessa/fractions/Frac.java
+++ src/main/java/com/inessa/fractions/Frac.java
@@ -1,4 +1,121 @@
 package com.inessa.fractions;
 
 public class Frac {
+    private int num;
+    private int den;
+
+    public Frac()
+    {
+        num = 0;
+        den = 1;
+    }
+
+    public Frac(int numinator, int denominator)
+    {
+        setNum(numinator);
+        setDen(denominator);
+    }
+
+    public void setNum(int num)
+    {
+        this.num = num;
+    }
+
+    public void setDen(int denom)
+    {
+        if (denom != 0)
+        {
+            den = denom;
+        }
+        else {
+            den = 1;
+        }
+    }
+
+    public int getNum()
+    {
+        return num;
+    }
+
+    public int getDen()
+    {
+        return den;
+    }
+
+    @Override
+    public String toString()
+    {
+        return num + "/" + den;
+    }
+
+    public Frac summ(Frac first_frac, Frac second_frac)
+    {
+        Frac rez = new Frac();
+        if (first_frac.getDen() != second_frac.getDen())
+        {
+            int new_den = first_frac.getDen() * second_frac.getDen();
+            int new_num1 = first_frac.getNum() * second_frac.getDen();
+            int new_num2 = second_frac.getNum() * first_frac.getDen();
+            rez.setDen(new_den);
+            rez.setNum(new_num1 + new_num2);
+        }
+        else
+        {
+            rez.setDen(first_frac.getDen());
+            int num1 = first_frac.getNum();
+            int num2 = second_frac.getNum();
+            rez.setNum(num1 + num2);
+        }
+        return rez;
+    }
+
+    public Frac comp(Frac first_frac, Frac second_frac)
+    {
+        Frac rez = new Frac();
+        int new_num = first_frac.getNum() * second_frac.getNum();
+        int new_den = first_frac.getDen() * second_frac.getDen();
+        rez.setNum(new_num);
+        rez.setDen(new_den);
+        return rez;
+    }
+
+    public Frac minus(Frac first_frac, Frac second_frac)
+    {
+        Frac rez = new Frac();
+        if (first_frac.getDen() != second_frac.getDen())
+        {
+            int new_den = first_frac.getDen() * second_frac.getDen();
+            int new_num1 = first_frac.getNum() * second_frac.getDen();
+            int new_num2 = second_frac.getNum() * first_frac.getDen();
+            rez.setDen(new_den);
+            rez.setNum(new_num1 - new_num2);
+        }
+        else
+        {
+            rez.setDen(first_frac.getDen());
+            int num1 = first_frac.getNum();
+            int num2 = second_frac.getNum();
+            rez.setNum(num1 - num2);
+        }
+        return rez;
+    }
+
+    public Frac divis(Frac first_frac, Frac second_frac)
+    {
+        Frac rez = new Frac();
+        int new_num = first_frac.getNum() * second_frac.getDen();
+        int new_den = first_frac.getDen() * second_frac.getNum();
+        if (new_den != 0)
+        {
+            rez.setNum(new_num);
+            rez.setDen(new_den);
+            return rez;
+        }
+        else
+        {
+            throw new RuntimeException();
+        }
+    }
+
+
 }
--- src/main/java/com/inessa/fractions/HelloController.java
+++ src/main/java/com/inessa/fractions/HelloController.java
@@ -1,14 +1,79 @@
 package com.inessa.fractions;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public TextField Num1;
+
+    @FXML
+    public TextField Num2;
+
+    @FXML
+    public TextField Den1;
+
+    @FXML
+    public TextField Den2;
+
+    @FXML
+    public TextField Ans;
+
+    public void onSumbClick()
+    {
+        try {
+            Frac first = new Frac(Integer.parseInt(Num1.getText()), Integer.parseInt(Den1.getText()));
+            Frac sec = new Frac(Integer.parseInt(Num2.getText()), Integer.parseInt(Den2.getText()));
+            Frac rez = new Frac().summ(first, sec);
+            Ans.setText(rez.toString());
+        }
+        catch (NumberFormatException ex)
+        {
+
+        }
     }
+
+    public void onMindClick()
+    {
+        try {
+        Frac first = new Frac(Integer.parseInt(Num1.getText()), Integer.parseInt(Den1.getText()));
+        Frac sec = new Frac(Integer.parseInt(Num2.getText()), Integer.parseInt(Den2.getText()));
+        Frac rez = new Frac().minus(first, sec);
+        Ans.setText(rez.toString());
+        }
+        catch (NumberFormatException ex)
+        {
+
+        }
+    }
+
+    public void onCompdClick()
+    {
+        try {
+        Frac first = new Frac(Integer.parseInt(Num1.getText()), Integer.parseInt(Den1.getText()));
+        Frac sec = new Frac(Integer.parseInt(Num2.getText()), Integer.parseInt(Den2.getText()));
+        Frac rez = new Frac().comp(first, sec);
+        Ans.setText(rez.toString());
+        }
+        catch (NumberFormatException ex)
+        {
+
+        }
+    }
+
+    public void onDivisdClick()
+    {
+        try {
+        Frac first = new Frac(Integer.parseInt(Num1.getText()), Integer.parseInt(Den1.getText()));
+        Frac sec = new Frac(Integer.parseInt(Num2.getText()), Integer.parseInt(Den2.getText()));
+        Frac rez = new Frac().divis(first, sec);
+        Ans.setText(rez.toString());
+        }
+        catch (NumberFormatException ex)
+        {
+
+        }
+    }
+
 }
